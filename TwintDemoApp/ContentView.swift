23e1c37e556b78714bda40e95ccb5b980ac71334
Marko Jovanov <marko.jovanov@netcetera.com>
2024-11-14 14:09:08 +0100

    Push

--- TwintDemoApp/ContentView.swift
+++ TwintDemoApp/ContentView.swift
@@ -25,8 +25,7 @@ struct ContentView: View {
     }
 
     private func openDemoTestThreeApp() {
-            if let url = URL(string: "map4vab://twint/authorize?client_id=eKNjzFFjH9A1ysYd&response_type=code&redirect_uri=twintvab://callback&state=1234zyx&scope=https://twint.swisscom.ch/scopes/registration&code_challenge=hKpKupTM381pE10yfQiorMxXarRKAHRhTfH_xkGf7U4&code_challenge_method=S256") {
-
+            if let url = URL(string: "map4vanilla://twint/authorize?client_id=dsadsa&response_type=code&redirect_uri=twintvanilla://callback&state=1234zyx&scope=https://test.ch/scopes/registration&code_challenge=dasdas&code_challenge_method=S256") {
                 if UIApplication.shared.canOpenURL(url) {
                     UIApplication.shared.open(url, options: [:], completionHandler: nil)
                 } else {
